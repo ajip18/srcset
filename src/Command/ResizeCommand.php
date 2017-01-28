e6ca8cbf9d2bfd4936b9af5143ae9a4ae7c97e3b
Levi Durfee <levi.durfee@gmail.com>
2017-01-28 12:03:33 -0500

    add filename argument

--- src/Command/ResizeCommand.php
+++ src/Command/ResizeCommand.php
@@ -2,9 +2,11 @@
 
 namespace wappr\Command;
 
+use wappr\Resize;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
 class ResizeCommand extends Command
 {
@@ -12,6 +14,7 @@ class ResizeCommand extends Command
     {
         $this
         ->setName('srcset')
+        ->addArgument('filename', InputArgument::REQUIRED, 'The filename to resize.')
         ->setDescription('Create images for srcset')
         ->setHelp('This command will create multiple images for your srcset.');
     }
@@ -19,5 +22,7 @@ class ResizeCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Resizing...');
+        $filename = $input->getArgument('filename');
+        $resizer = new Resize($filename);
     }
 }
--- src/Resize.php
+++ src/Resize.php
@@ -4,5 +4,10 @@ namespace wappr;
 
 class Resize
 {
+	public $filename;
     public $imagesize;
+
+    public function __construct($filename) {
+    	$this->filename = $filename;
+    }
 }
