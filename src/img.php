ec8f025869009cc59c9392f15b3e0ee51f9b96af
Levi Durfee <levi.durfee@gmail.com>
2017-02-04 15:55:44 -0500

    add static srcset sizes

--- src/img.php
+++ src/img.php
@@ -1,7 +1,11 @@
 <?php
 
 $html = '
-<img sizes="100vw" 
+<img sizes="(max-width: 273px) 200px,
+            (max-width: 299px) 274px,
+            (max-width: 334px) 320px,
+            (max-width: 759px) 335px,
+            760px" 
 			srcset="' .$images.'" 
 			src="' .$image.'" 
 			alt="' .$alt.'">
