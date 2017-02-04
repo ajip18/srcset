8f3ce6d32a6a08c08e529a7b99a4d0a3a614b524
Levi Durfee <levi.durfee@gmail.com>
2017-02-04 16:58:18 -0500

    change sizes

--- src/img.php
+++ src/img.php
@@ -3,10 +3,6 @@
 $html = '
 <img src="' .$image.'" 
 srcset="' .$images.'" 
-sizes="(max-width: 273px) 200px,
-(max-width: 299px) 274px,
-(max-width: 334px) 320px,
-(max-width: 759px) 335px,
-760px" 
+sizes="100vw" 
 alt="' .$alt.'">
 ';
